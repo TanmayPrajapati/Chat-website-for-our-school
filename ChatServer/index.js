95072ea4c5998f7fe26c8f914adf645b8d130455
Tanmay <prajapatitanmay977@gmail.com>
2020-06-10 18:57:56 +0530

    initial

--- ChatServer/index.js
+++ ChatServer/index.js
@@ -4,26 +4,20 @@ const members = [];
 
 
 io.on("connection" , (socket)=>{
-console.log("Connected");
 socket.on("addNewMember", name=>{
 
 members[socket.id]= name;
-console.log("New user joined:", name);
 
 socket.broadcast.emit("NewUserJoined", name)
 
 });
 
 socket.on("send", function (msg){
-	console.log(msg);
 	socket.broadcast.emit("receive", {message:msg, name:members[socket.id]});
 });
 
 socket.on("disconnect", function(){
-	console.log(members);
 	delete members[socket.id];
-	console.log("removed");
-	console.log(members);
 });
 
 
